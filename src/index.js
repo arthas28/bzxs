13330e1f4308996bce5155c458084fbeb8732fcf
yangjunjie <yangjunjie@qq.com>
2017-01-24 15:05:14 +0800

    modify 1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import Vue from "vue"; //会去node_modules\vue\package.json
 
 new Vue({
     el: '#indexWrapHome',
-    data: { myTime: '2017-01-01' }
+    data: { myTime: '2017-01-03' }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,11 @@ module.exports =
     entry:
     {
         //入口文件
-        "index":__dirname+'/src/index.js',
+        "index":__dirname+'/src/index.js'
     },
     output: {
         path: __dirname+'/build/js',  //输出文件夹
+        //publicPath: '/build/',
         filename:'[name].js'   //最终打包生成的文件名(只是文件名，不带路径的哦)
     },
     resolve: {
@@ -21,9 +22,18 @@ module.exports =
     externals: {
 
     },
+    devServer: {
+        contentBase: 'build',  //静态资源的目录 相对路径,相对于当前路径 默认为当前config所在的目录
+        historyApiFallback: true,
+        noInfo: true
+        //port: 3005 
+    },
+    performance: {
+        hints: false
+    },
     module:{
         loaders:[
-            {test:/\.js$/,loader:"babel",query:{compact:true}},
+            {test:/\.js$/, loader:"babel-loader", query:{compact:true}},
             //这里肯定要加入n个loader 譬如vue-loader、babel-loader、css-loader等等
         ]
     },
@@ -35,6 +45,5 @@ module.exports =
             hash:true,  //代表js文件后面会跟一个随机字符串,解决缓存问题
             chunks:["index"]
         })
-
     ]
 }
